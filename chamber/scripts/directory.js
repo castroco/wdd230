93a94f990718213d86958ec6b43d3d3f12321d3d
castroco <carlos.castro.p@gmail.com>
2022-11-25 21:45:38 -0300

    directory 202211252145

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,46 @@
+const jsonData = "https://castroco.github.io/wdd230/chamber/info/data.json";
+/*cardsContainer = document.querySelector('div.cards');*/
+
+fetch(jsonData).then(function (response) {
+    return response.json();
+})
+    .then(function (jsonObject) 
+        {
+            console.log("jsonObjectCompaniesData: ", jsonObject);
+            companies = jsonObject['companies'];
+            console.log("companies: ", companies);
+            /*companies.forEach(displayCompanies);*/
+        }
+    );
+
+/*function displayCompanies(company) {  
+    let card = document.createElement('section');
+    let image = document.createElement('img');
+    let name = document.createElement('p');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let website = document.createElement('p');
+    
+    name.textContent = `${company.name}`;
+    website.textContent = `${company.website}`;
+    address.textContent = `${company.address}`;
+    phone.textContent = `${company.phone}`;
+    image.setAttribute('src', company.logo);
+    image.setAttribute('alt', `Logo of the company`);
+    card.appendChild(image);
+    card.appendChild(name);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(website);
+    cardsContainer.appendChild(card);
+}
+
+const cardsButton = document.querySelector("#cardsIcon");
+cardsButton.addEventListener("click", () => {
+    cardsContainer.classList.replace("displayList","displayCards");
+});
+
+const listButton = document.querySelector("#listIcon");
+listButton.addEventListener("click", () => {
+    cardsContainer.classList.replace("displayCards","displayList");
+});*/
